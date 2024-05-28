59749b9f244d50956ce4f0f41381b21f66672f0b
atmiktumane <atmiktumane72@gmail.com>
2024-05-28 10:26:29 +0530

    created practice project with tailwind css

---
+++ index.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    darkmode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                "logo": {
+                    700: "#1C59AE",
+                    800: "#114a9a",
+                }
+            }
+        }
+    }
+}
